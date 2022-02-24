6d3d3612a07c5ee2f9dddf63a7b4d75b09cdc4ab
Alex Adamovic <alexanderadamovic@gmail.com>
2022-02-24 10:46:32 -0800

    add second test for WordScore property Word
    Co-authored-by: Will Greenberg <mud2009@users.noreply.github.com>
    Co-authored-by: marisa-edgar <marisa-edgar@users.noreply.github.com>

--- ScrabbleScore.Tests/Model.Tests/WordScoreTests.cs
+++ ScrabbleScore.Tests/Model.Tests/WordScoreTests.cs
@@ -12,5 +12,14 @@ namespace ScrabbleScore.Tests
       WordScore newWordScore = new WordScore();
       Assert.AreEqual(typeof(WordScore), newWordScore.GetType());
     }
+
+    [TestMethod]
+    public void WordScoreConstructor_ReturnWordVariable_String()
+    {
+      string test = "test";
+      WordScore newWordScore = new WordScore(test);
+      string result = newWordScore.Word;
+      Assert.AreEqual(test,result);
+    }
   }
 }
